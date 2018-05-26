d27ae034ed10594154968b312390d07b62f569ae
AndrewAdam1989 <36536198+AndrewAdam1989@users.noreply.github.com>
2018-05-26 16:33:17 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 php-fpm7.0 --daemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf
 sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
-sed -i "s/ALTER_ID/$ALTER_ID/g" /etc/v2ray/config.json
-sed -i "s#WS_PATH#$WS_PATH#g" /etc/v2ray/config.json
-sed -i "s#WS_PATH#$WS_PATH#g" /etc/nginx/conf.d/default.conf
+sed -i "s/ALTERID/$ALTERID/g" /etc/v2ray/config.json
+sed -i "s#PATH#$PATH#g" /etc/v2ray/config.json
+sed -i "s#PATH#$PATH#g" /etc/nginx/conf.d/default.conf
 v2ray -config=/etc/v2ray/config.json &
 nginx -g 'daemon off;'
